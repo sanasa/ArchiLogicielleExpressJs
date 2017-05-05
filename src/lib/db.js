eb2547ff76c0e38b743e085e54dd8eeba46c9a87
Sana KHIARI <khiarisanaa@gmail.com>
2017-05-05 21:34:35 +0100

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 //var mongoUrl = "mongodb://sana:Allah7akbar@ds115071.mlab.com:15071/movies";
 //var mongoUrl =process.env.MONGO_URL ||'mongodb://sana:sana@127.0.0.1:27017/movies';
 //mongodb://<user>:<pass>@mongo.onmodulus.net:27017/uw15dfpu
-var mongoUrl =process.env.MONGO_URL || 'mongodb://localhost/movies';
+var mongoUrl =process.env.MONGOLAB_URI;
 module.exports = function(app) {
   mongoose.Promise = global.Promise;
   mongoose.connect(mongoUrl, {
